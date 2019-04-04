f2c7fba773c290d43658ebf5e771c39ecb82f454
jason.chang <jason.chang@gogolook.com>
2019-04-04 18:07:08 +0800

    implementation of single pattern

--- class2/singleton.py
+++ class2/singleton.py
@@ -1,12 +1,31 @@
-i#!/usr/bin/env python3
+#!/usr/bin/env python3
 
 class DBConnection:
-    pass
+    single = None
+
+    def __init__(self):
+        if DBConnection.single:
+            raise DBConnection.single
+        DBConnection.single = self
+
+    @staticmethod
+    def get_instance():
+        if not DBConnection.single:
+            DBConnection.single = DBConnection()
+        return DBConnection.single
+
+    def query(self):
+        print('query now')
 
 
 if __name__ == '__main__':
     conn = DBConnection.get_instance()  # print "connect to db"
     conn.query()  # print "Make a query"
 
-    conn = DBConnection.get_instance()  # print nothing
+    newConn = DBConnection.get_instance()  # print nothing
     conn.query()  # print "Make a query"
+
+    if conn is newConn:
+        print('is the same object')
+    else:
+        print('not same object')
